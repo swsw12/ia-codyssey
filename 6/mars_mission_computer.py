ec8220dd326154291d510cabf9536ce0a50369dc
swsw12 <163833032+swsw12@users.noreply.github.com>
2025-06-05 21:58:57 +0900

    Create mars_mission_computer.py

---
+++ 6/mars_mission_computer.py
@@ -0,0 +1,31 @@
+import random
+
+class DummySensor:
+    def __init__(self):
+        self.env_values = {
+            'mars_base_internal_temperature': 0,
+            'mars_base_external_temperature': 0,
+            'mars_base_internal_humidity': 0,
+            'mars_base_external_illuminance': 0,
+            'mars_base_internal_co2': 0,
+            'mars_base_internal_oxygen': 0,
+        }
+
+    def set_env(self):
+        self.env_values['mars_base_internal_temperature'] = random.uniform(18, 30)
+        self.env_values['mars_base_external_temperature'] = random.uniform(0, 21)
+        self.env_values['mars_base_internal_humidity'] = random.uniform(50, 60)
+        self.env_values['mars_base_external_illuminance'] = random.uniform(500, 715)
+        self.env_values['mars_base_internal_co2'] = random.uniform(0.02, 0.1)
+        self.env_values['mars_base_internal_oxygen'] = random.uniform(4, 7)
+
+    def get_env(self):
+        return self.env_values
+
+
+if __name__ == '__main__':
+    ds = DummySensor()
+    ds.set_env()
+    env = ds.get_env()
+    for key, value in env.items():
+        print(f'{key}: {value:.3f}')
