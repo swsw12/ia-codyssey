1f584e0040aba72b0fc73245a36075b1e4a1c5d3
swsw12 <163833032+swsw12@users.noreply.github.com>
2025-06-07 01:56:22 +0900

    Create mars_mission_computer.py

---
+++ 7/mars_mission_computer.py
@@ -0,0 +1,51 @@
+import time
+import json
+import random
+
+
+class DummySensor:
+    def __init__(self):
+        self.env_values = {
+            'mars_base_internal_temperature': 0,
+            'mars_base_external_temperature': 0,
+            'mars_base_internal_humidity': 0,
+            'mars_base_external_illuminance': 0,
+            'mars_base_internal_co2': 0,
+            'mars_base_internal_oxygen': 0
+        }
+
+    def set_env(self):
+        self.env_values['mars_base_internal_temperature'] = random.uniform(18, 30)
+        self.env_values['mars_base_external_temperature'] = random.uniform(0, 21)
+        self.env_values['mars_base_internal_humidity'] = random.uniform(50, 60)
+        self.env_values['mars_base_external_illuminance'] = random.uniform(500, 715)
+        self.env_values['mars_base_internal_co2'] = random.uniform(0.02, 0.1)
+        self.env_values['mars_base_internal_oxygen'] = random.uniform(4, 7)
+
+    def get_env(self):
+        return self.env_values
+
+
+class MissionComputer:
+    def __init__(self):
+        self.env_values = {
+            'mars_base_internal_temperature': 0,
+            'mars_base_external_temperature': 0,
+            'mars_base_internal_humidity': 0,
+            'mars_base_external_illuminance': 0,
+            'mars_base_internal_co2': 0,
+            'mars_base_internal_oxygen': 0
+        }
+        self.ds = DummySensor()
+
+    def get_sensor_data(self):
+        while True:
+            self.ds.set_env()
+            self.env_values = self.ds.get_env()
+            print(json.dumps(self.env_values, indent=4))
+            time.sleep(5)
+
+
+if __name__ == '__main__':
+    RunComputer = MissionComputer()
+    RunComputer.get_sensor_data()
